80bfa9d5f238b27b6fa9a63908b50fa3c03bf877
Zubin-glitch <zubintobias@gmail.com>
2025-04-05 18:03:31 -0700

    Added comparison of cards and correct functionality for the deck.

--- card.py
+++ card.py
@@ -1,12 +1,14 @@
 class Card:
-    valid_ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    valid_suits = ["club", "diamond", "heart", "spade"]
+    static_ranks = [str(n) for n in range(2, 11)] + ['J', 'Q', 'K', 'A']
+    static_suites = ["club", "diamond", "heart", "spade"]
+    # Added for testing purposes, TODO: Remove global_deck
     global_deck = []
+
     # Constructor of the class
     def __init__(self, rank: str, suit: str):
         # Check validity of the rank and suit
-        assert rank.upper() in Card.valid_ranks, f"{rank} Rank is not valid! Valid ranks are: {Card.valid_ranks}"
-        assert suit.lower() in Card.valid_suits, f"{suit} Suit isn't valid! It should be: {Card.valid_suits}"
+        assert rank.upper() in Card.static_ranks, f"{rank} Rank is not valid! Valid ranks are: {Card.static_ranks}"
+        assert suit.lower() in Card.static_suites, f"{suit} Suite isn't valid! It should be: {Card.static_suites}"
         
         # Card has two fields
         self.rank = rank.upper()
@@ -15,24 +17,87 @@ class Card:
         # Add card to global list of all instances
         Card.global_deck.append(self)
     
-    def __lt__(self, card):
-        pass
-    def get_rank(self):
-        return self.rank
+    # Some methods to order the cards
+    def __sub__(self, other):
+        """
+        Returns current card's rank minus another card's rank.
+        If current card has higher rank, returns a +ve integer
+        Else, other card has higher rank, return a -ve integer
+        """
+        if self.rank == 'A' and other.rank == '2':
+            return 1
+        elif self.rank == '2' and other.rank == 'A':
+            return -1
+        else:
+            return Card.static_ranks.index(self.rank) - Card.static_ranks.index(other.rank)
     
-    def get_suit(self):
-        return self.suit
+    def __lt__(self, other) -> bool:
+        return Card.static_ranks.index(self.rank) < Card.static_ranks.index(other.rank)
+    
+    def __le__(self, other) -> bool:
+        return Card.static_ranks.index(self.rank) <= Card.static_ranks_index(other.rank)
+    
+    def __gt__(self, other) -> bool:
+        return Card.static_ranks.index(self.rank) > Card.static_ranks.index(other.rank)
+    
+    def __ge__(self, other) -> bool:
+        return Card.static_ranks.index(self.rank) >= Card.static_ranks.index(other.rank)
+    
+    def __eq__(self, other) -> bool:
+        return Card.static_ranks.index(self.rank) == Card.static_ranks.index(other.rank)
+    
+
 
     def __str__(self):
-        return f"Card({self.rank} {self.suit})"
+        """
+        Returns: String representation of the card
+        """
+        view_card = f"{self.rank}"
+        match self.suit:
+            case "club":
+                view_card += '\u2663'
+            case "diamond":
+                view_card += '\u2666'
+            case "heart":
+                view_card += '\u2665'
+            case "spade":
+                view_card += '\u2660'
+            case _:
+                raise ValueError("Invalid Suite entered!")
+        return view_card
 
     def __repr__(self):
-        return f"({self.rank}, {self.suit})"
+        """
+        Returns a string that can be used to recreate the object.
+        This is the same as str(self).
+        """
+        return str(self)
+    
+    def compare(self, other):
+        """
+        This function compares two cards.
+        Returns : 1 (first card is higher in rank than second)
+        Returns : 0 (first card and second card have same rank)
+        Returns : -1 (first card is lower than second in rank)
+        """
+        if self > other:
+            return 1
+        elif self == other:
+            return 0
+        else:
+            return -1
 
+# Unit tests
 # card1 = Card('J', 'club')
 # print(card1)
 
 # card2 = Card('a', 'heart')
 # print(card2)
 
+# card3 = Card('A', 'spade')
+
+# print(card1 - card2)
+# print(card2.compare(card1))
+# print(card1.compare(card2))
+# print(card2.compare(card3))
 # print(Card.global_deck)
--- deck.py
+++ deck.py
@@ -1,46 +1,57 @@
-from card import Card
+from  card import Card
 import random
 # Deck is of 52 cards
 class Deck:
+    """
+    The Deck class is very simple, it is simply a list of Cards. 
+    The Deck constructor takes no arguments, 
+    the deck will contain one Card of every suite and every value.
+    """
     def __init__(self):
         # Create a deck of 52 cards whenever a new Deck object is initialized
-        self.cards = self.create_deck()
-        self.drawn_pile = []
+        self._deck = self.create_deck()
+        self.drawn_cards = []
     
     def create_deck(self):
         deck = []
-        for suit in Card.valid_suits:
-            for rank in Card.valid_ranks:
-                deck.append(Card(rank, suit))
+        for suite in Card.static_suites:
+            for rank in Card.static_ranks:
+                deck.append(Card(rank, suite))
         if len(deck) == 52:
             return deck
         else:
-            raise Exception('Debug, number of cards is not 52!')
+            raise Exception('Debug please, number of cards is not 52!')
 
     def shuffle(self):
-        print(f'Original deck = {self.cards}')
-        random.shuffle(self.cards)
-        # self.show_pile()
-        print(f'\n \nDeck after shuffling = {self.cards}')
+        print(f'Original deck = {self._deck}')
+        # Randomly shuffle the deck between 1 and 5 times.
+        # To mimic actual shuffling
+        for i in range(random.randint(1, 5)):
+            random.shuffle(self._deck)
+        print(f'\n \nDeck after shuffling = {self._deck}')
 
-    def show_pile(self):
-        for each_card in self.cards:
-            print(f'{each_card}')
-
-    def show_drawn_pile(self):
-        for each_card in self.drawn_pile:
-            print(f'{each_card}')
-
-    def draw(self):
-        # Pull card from top of deck
-        drawn_card = self.cards.pop()
-        # insert this card into the drawn pile
-        self.drawn_pile.append(drawn_card)
+    # This method wouldn't be needed since these cards should be hidden
+    def show_drawn_cards(self):
+        # print("Cards on the table are: ")
+        for each_card in self.drawn_cards:
+            print(each_card)
         
+    def __len__(self):
+        return len(self._deck)
+    
+    def deal_card(self):
+        drawn_card = self._deck.pop()
+        self.drawn_cards.append(drawn_card)
+        return drawn_card
+    def __str__(self):
+        return " ".join([str(card) for card in self._deck])
+    
 
-deck_1 = Deck()
-deck_1.shuffle()
-deck_1.draw()
+# Unit tests
+deck = Deck()
+print(deck)
+print(len(deck))
 
-deck_1.show_drawn_pile()
-deck_1.show_pile()
+deck.shuffle()
+dealt_card = deck.deal_card()
+print(f"Dealt card = {dealt_card} and size of the deck is = {len(deck)}")
